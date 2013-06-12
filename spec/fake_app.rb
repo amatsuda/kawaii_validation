69bedddc8389231d87c13f7e9545543ea004823f
Akira Matsuda <ronnie@dio.jp>
2013-06-12 19:46:29 +0900

    Prepare for specs

---
+++ spec/fake_app.rb
@@ -0,0 +1,18 @@
+# config
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
+
+module KawaiiValidationTestApp
+  Application = Class.new(Rails::Application) do
+    config.active_support.deprecation = :log
+  end.initialize!
+end
+
+# models
+class User < ActiveRecord::Base; end
+
+# migrations
+class CreateAllTables < ActiveRecord::Migration
+  def self.up
+    create_table(:users) {|t| t.string :name; t.string :email; t.integer :age}
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
+$LOAD_PATH.unshift(__dir__)
+
+require 'rails'
+require 'kawaii_validation'
+require 'fake_app'
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.before :all do
+    CreateAllTables.up unless ActiveRecord::Base.connection.table_exists? 'users'
+  end
+  config.before :each do
+    User.delete_all
+  end
+end
